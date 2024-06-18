a071c573b331b107171079f7b1bab8b7e64736a6
Juan Carlos <leizar999@gmail.com>
2024-06-18 22:46:19 +0200

    Added some selectors to the project

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'countries-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+
+  public countriesByRegion: SmallCountry[] = [];
+
   public myForm: FormGroup = this.fb.group({
     region:  ['', Validators.required],
     country: ['', Validators.required],
@@ -20,8 +24,23 @@ export class SelectorPageComponent {
     private countriesService: CountriesService
   ) {}
 
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  public onRegionChanged(): void {
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),
+        switchMap( (region) => this.countriesService.getCountriesByRegion(region) )
+      )
+      .subscribe( countries => {
+        this.countriesByRegion = countries;
+      });
   }
 
   public onSubmit() {
